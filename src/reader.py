727b9a18f95aab9dcc85de4e5e431d2b69c5d940
toxpy <natoxpy@gmail.com>
2024-09-08 12:31:29 -0400

    feat: pdf reader

--- src/reader.py
+++ src/reader.py
@@ -1,3 +1,15 @@
 import pymupdf
- 
+import re
 
+
+def read_pdf(file: str, page_start: int = 0, page_end: int = None):
+    doc = pymupdf.open(file)
+    content = ""
+
+    if not page_end:
+        page_end = len(doc)
+
+    for i in range(page_start, page_end + 1):
+        content = content + " " + doc[i - 1].get_text()
+
+    return " ".join(re.split("\s+", content, flags=re.UNICODE)).strip()
